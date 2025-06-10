a4d25ce4fb0655dda9ebb25b9f5ad477e6095b79
Arbeshab <160234881+Arbeshab@users.noreply.github.com>
2025-06-10 15:33:50 +0200

    Add files via upload

---
+++ VHDL_code/complete_system/ipcore_dir/ipcore_dir_synth/lut_sine_coarse_128_vals/simulation/functional/wave_ncsim.sv
@@ -0,0 +1,18 @@
+
+
+ 
+ 
+ 
+
+
+
+
+window new WaveWindow  -name  "Waves for BMG Example Design"
+waveform  using  "Waves for BMG Example Design"
+
+      waveform add -signals /lut_sine_coarse_128_vals_tb/status
+      waveform add -signals /lut_sine_coarse_128_vals_tb/lut_sine_coarse_128_vals_synth_inst/bmg_port/CLKA
+      waveform add -signals /lut_sine_coarse_128_vals_tb/lut_sine_coarse_128_vals_synth_inst/bmg_port/ADDRA
+      waveform add -signals /lut_sine_coarse_128_vals_tb/lut_sine_coarse_128_vals_synth_inst/bmg_port/DOUTA
+
+console submit -using simulator -wait no "run"
